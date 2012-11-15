a70ec727fcff2bc1dab3221b656e1df32b40fe2d
mohanraj.r <mohanraj.r@payoda.com>
2012-11-15 18:08:14 +0530

    New

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   #This is for new branch
+  #This is for new branch
   protect_from_forgery
 
   before_filter :authenticate_user!, :redirect_www_to_normal
