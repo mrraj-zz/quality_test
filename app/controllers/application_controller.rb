920c1fb3f34dd2e7a44d633e69c3864353e2968d
mohanraj.r <mohanraj.r@payoda.com>
2012-11-15 16:27:48 +0530

    Created new branch with changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  #This is for new branch
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :redirect_www_to_normal
+
+  def redirect_www_to_normal
+    redirect_to request.url.gsub(/www./, "") unless request.url.scan(/\/\/www./).blank?
+  end
+
+  def render_404
+    render :template => "errors/404", :format => [:html], :layout => "application"
+  end
 end
